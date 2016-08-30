2db352d7d37423077fef20de9bbe37ac5b173b08
cw <284619466@qq.com>
2016-08-31 00:33:43 +0800

    我做的第一个react native 应用

--- src/component/Iosui.js
+++ src/component/Iosui.js
@@ -7,10 +7,12 @@ import TabNavigator from 'react-native-tab-navigator';
 
 import About from '../menu/About';
 import Home from '../menu/Home';
+import More from '../yinguo/more/More';
 
 const tabBarItems = [
-    { title: '最新', icon: () => <Image style={{ width: 30, height: 30 }} source={require('../png/icon/home.png')}/>, component: Home },
-    { title: '分类', icon: () => <Image style={{ width: 30, height: 30 }} source={require('../png/icon/me.png')}/>, component: About },
+    { title: '动态', icon: () => <Image style={{ width: 30, height: 30 }} source={require('../png/icon/home.png')}/>,select_icon: () => <Image style={{ width: 30, height: 30 }} source={require('../png/icon/home_selected.png')}/>, component: Home },
+    { title: '好友', icon: () => <Image style={{ width: 30, height: 30 }} source={require('../png/icon/friend.png')}/>,select_icon: () => <Image style={{ width: 30, height: 30 }} source={require('../png/icon/friend_selected.png')}/>, component: About },
+    { title: '更多', icon: () => <Image style={{ width: 30, height: 30 }} source={require('../png/icon/more.png')}/>,select_icon: () => <Image style={{ width: 30, height: 30 }} source={require('../png/icon/more_selected.png')}/>, component: More },
 ];
 
 export default class Iosui extends Component{
@@ -22,9 +24,22 @@ export default class Iosui extends Component{
               selectedTab: tabBarItems[0].title,
           };
       }
+
+      renderBadge(){
+          return(
+              <View style={{width:20,height:20,borderRadius:10,backgroundColor:'red',alignItems:'center',justifyContent:'center'}}>
+                  <Text style={{}}>
+                      2
+                  </Text>
+              </View>
+          );
+      }
+
     render() {
         return (
-            <TabNavigator tabBarStyle={{ height: 60 }}>
+            <TabNavigator tabBarStyle={{ height: 55,backgroundColor:'bisque'}}
+                          allowFontScaling={false}
+            >
                 {
                     tabBarItems.map((controller, i) => {
                         let Component = controller.component;
@@ -33,7 +48,10 @@ export default class Iosui extends Component{
                                 key= {i}
                                 selected={this.state.selectedTab === controller.title}
                                 title={controller.title}
+                                selectedTitleStyle={{color:'red',fontSize:10,fontWeight:'bold'}}
                                 renderIcon={controller.icon}
+                                renderSelectedIcon={controller.select_icon}
+                                renderBadge={()=> this.renderBadge()}
                                 onPress={() => this.setState({ selectedTab: controller.title }) }>
                                 <Component navigator = {this.props.navigator} {...this.props}/>
                             </TabNavigator.Item>
@@ -44,4 +62,5 @@ export default class Iosui extends Component{
 
         );
     }
+    //exnavigator可以作为替代
 }
---
+++ src/yinguo/more/More.js
@@ -0,0 +1,23 @@
+/**
+ * Created by cw on 16/8/30.
+ */
+
+import React,{Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class More extends Component{
+    // 构造
+      constructor(props) {
+        super(props);
+        // 初始状态
+        this.state = {};
+      }
+
+      render(){
+          return(
+              <View>
+                  <Text>111</Text>
+              </View>
+          );
+      }
+}
