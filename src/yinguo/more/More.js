dbe928be9b5660b802d3dd9423a80a1c83ba18d8
cw <284619466@qq.com>
2016-08-31 23:51:58 +0800

    我做的第一个react native 应用

--- src/yinguo/more/More.js
+++ src/yinguo/more/More.js
@@ -13,21 +13,25 @@ import YinguoToolBar from '../component/YinguoToolBar';
 
 const DATA = [
     {name:'主页', image:require('../../png/yinguo/search.png'), isMain:false, isLast:false, onpress:{}},
-    {name:'邀请朋友,获取奖金', image:require('../../png/yinguo/search.png'), isMain:false,isLast:true, onpress:{}},
-    {name:'公共主页', image:require('../../png/yinguo/search.png'), isMain:true, isLast:false,onpress:{}},
-    {name:'创建主页', image:require('../../png/yinguo/search.png'), isMain:false, isLast:false,onpress:{}},
-    {name:'管理主页', image:require('../../png/yinguo/search.png'), isMain:false, isLast:true,onpress:{}},
-    {name:'社群', image:require('../../png/yinguo/search.png'), isMain:true, isLast:false,onpress:{}},
-    {name:'创建社群', image:require('../../png/yinguo/search.png'), isMain:false, isLast:false,onpress:{}},
-    {name:'管理社群', image:require('../../png/yinguo/search.png'), isMain:false, isLast:true,onpress:{}},
-    {name:'常用功能', image:require('../../png/yinguo/search.png'), isMain:true, isLast:false,onpress:{}},
-    {name:'活动', image:require('../../png/yinguo/search.png'), isMain:false, isLast:false,onpress:{}},
-    {name:'发现好友', image:require('../../png/yinguo/search.png'), isMain:false, isLast:false,onpress:{}},
-    {name:'收藏夹', image:require('../../png/yinguo/search.png'), isMain:false, isLast:false,onpress:{}},
+    {name:'邀请朋友,获取奖金', image:require('../../png/yinguo/friend.png'), isMain:false,isLast:true, onpress:{}},
+
+    {name:'公共主页', isMain:true, isLast:false,onpress:{}},
+    {name:'创建主页', image:require('../../png/yinguo/establishmain.png'), isMain:false, isLast:false,onpress:{}},
+    {name:'管理主页', image:require('../../png/yinguo/managemain.png'), isMain:false, isLast:true,onpress:{}},
+
+    {name:'社群', isMain:true, isLast:false,onpress:{}},
+    {name:'创建社群', image:require('../../png/yinguo/establishcommunity.png'), isMain:false, isLast:false,onpress:{}},
+    {name:'管理社群', image:require('../../png/yinguo/managecommunity.png'), isMain:false, isLast:true,onpress:{}},
+
+    {name:'常用功能', isMain:true, isLast:false,onpress:{}},
+    {name:'活动', image:require('../../png/yinguo/activity.png'), isMain:false, isLast:false,onpress:{}},
+    {name:'发现好友', image:require('../../png/yinguo/findfriends.png'), isMain:false, isLast:false,onpress:{}},
+    {name:'收藏夹', image:require('../../png/yinguo/collections.png'), isMain:false, isLast:false,onpress:{}},
     {name:'添加一些常用功能', image:null, isMain:false, isLast:true,onpress:{}},
-    {name:'', image:require('../../png/yinguo/search.png'), isMain:true, isLast:false,onpress:{}},
-    {name:'设置', image:require('../../png/yinguo/search.png'), isMain:false, isLast:false,onpress:{}},
-    {name:'快捷设置', image:require('../../png/yinguo/search.png'), isMain:false, isLast:true,onpress:{}},
+
+    {name:'', isMain:true, isLast:false,onpress:{}},
+    {name:'设置', image:require('../../png/yinguo/setting.png'), isMain:false, isLast:false,onpress:{}},
+    {name:'快捷设置', image:require('../../png/yinguo/fastsetting.png'), isMain:false, isLast:true,onpress:{}},
 
 ];
 
